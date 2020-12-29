2a1edbcea78b483893e48bcc4f1a9c21a4bff439
X996CMC <maigaard.christian@gmail.com>
2020-12-29 15:00:48 +0100

    created basic ASP.net core API

---
+++ Kraftvaerk-shop-server/Kraftvaerk-shop-server/Models/KraftvaerkContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Kraftvaerk_shop_server.Models
+{
+    public class KraftvaerkContext : DbContext
+    {
+        public KraftvaerkContext(DbContextOptions<KraftvaerkContext> options)
+             : base(options)
+        {
+        }
+
+        public DbSet<Product> Products
+        {
+            get; set;
+        }
+    }
+}
+
+
+
+
+
---
+++ Kraftvaerk-shop-server/Kraftvaerk-shop-server/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kraftvaerk_shop_server.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public string MediaUrl { get; set; }
+    }
+}
